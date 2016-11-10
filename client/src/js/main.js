358544935fc477ba7313aa31f9e377b46ee38c95
Siddharth11 <siddharth2parmar@gmail.com>
2016-11-11 01:05:14 +0530

    update folder structure

--- client/src/js/main.js
+++ client/src/js/main.js
@@ -6,6 +6,8 @@ import store from './store'
 import { Provider } from 'react-redux'
 import clientSideEvents from './clientSideEvents'
 
+import styles from './../scss/main.scss'
+
 const wrapper = document.querySelector('.page-wrapper')
 
 render(
